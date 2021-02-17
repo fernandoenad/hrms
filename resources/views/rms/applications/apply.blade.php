9e7bbbe2575b1c612759f4125174974faec15464
fernandoenad <fernando.enad@deped.gov.ph>
2021-02-17 09:53:34 +0800

    Update- fixes

--- resources/views/rms/applications/apply.blade.php
+++ resources/views/rms/applications/apply.blade.php
@@ -76,7 +76,7 @@
                                 <div class="col-md-8">
                                     <input id="pertdoc_soft" type="file" class="form-control-file @error('pertdoc_soft') is-invalid @enderror" name="pertdoc_soft" value="{{ old('pertdoc_soft') }}" autocomplete="pertdoc_soft">
                                     <small><em>Refer to the memorandum relevant to the job opening for details.</em></small>
-                                    <small><em class="text-danger">
+                                    <small><em class="text-mute">
                                         <br>
                                         Note: If your file is more than 2MB, please only submit a scanned copy of your 
                                         omnibus certification, and email the rest of your pertinent papers (<strong>in a single
