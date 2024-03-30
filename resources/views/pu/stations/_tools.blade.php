78383fd88868a6a977a0f6ac1e915b318cf23863
Fernando Enad <31582634+fernandoenad@users.noreply.github.com>
2024-03-30 16:53:50 +0800

    Update _tools.blade.php

--- resources/views/pu/stations/_tools.blade.php
+++ resources/views/pu/stations/_tools.blade.php
@@ -7,7 +7,7 @@
                 <form class="form-inline" method="post" action="{{ route('pu.stations.search') }}">
                     @csrf
                     <div class="input-group input-group-md">
-                        <input id="searchString" name="searchString" class="form-control form-control-navbar @error('searchString') is-invalid @enderror" value="{{ old('searchString') ?? request()->get('searchString') }}" autocomplete="searchString" type="search" placeholder="Search station" aria-label="Search">
+                        <input id="searchString" name="searchString" class="form-control form-control-navbar @error('searchString') is-invalid @enderror" value="{{ old('searchString') ?? request()->get('searchString') }}" autocomplete="searchString" type="search" placeholder="Search office" aria-label="Search">
                         <div class="input-group-append">
                             <button class="btn btn-primary" type="submit">
                                 <i class="fas fa-search"></i>
