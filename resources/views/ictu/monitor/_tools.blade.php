51365edde0f0e1e3dbdf011733a87945c0f0262a
Fernando Enad <31582634+fernandoenad@users.noreply.github.com>
2025-03-01 14:12:08 +0800

    Update _tools.blade.php

--- resources/views/ictu/monitor/_tools.blade.php
+++ resources/views/ictu/monitor/_tools.blade.php
@@ -4,7 +4,7 @@
     <div class="card-body p-0">
         <ul class="nav nav-pills flex-column">
             <li class="nav-item active p-3">
-               
+               <a href="{{ route('ictu.monitor') }}">Refresh</a>
             </li>
         </ul>
     </div>
