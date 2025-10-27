1ef8417fcbd5d7a9f59ab2aa960f6049c7039377
Fernando Enad <31582634+fernandoenad@users.noreply.github.com>
2025-10-28 05:31:59 +0800

    Update 503.blade.php

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -10,7 +10,7 @@
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="{{ route('my') }}">Home</a></li>
-                    <li class="breadcrumb-item active">401 Security Error</li>
+                    <li class="breadcrumb-item active">503 Error</li>
                 </ol>
             </div>
         </div>
@@ -20,7 +20,7 @@
 <div class="container">
     <div class="row">
         <div class="error-page">
-            <h2 class="headline text-danger">503</h2>
+            <h2 class="headline text-warning">503</h2>
 
             <div class="error-content">
                 <h3><i class="fas fa-exclamation-triangle text-warning"></i>We’ve Upgraded!</h3>
